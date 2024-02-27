8effe566ff15beb4a99234b3b2fb804653ae0ea1
Igorsiete <igorsiete@hotmail.es>
2024-02-27 21:09:17 +0100

    Mejorado
    Un pequeño cambio

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ function anadir(){
 }
 var cadena="";
 var la="";
+var m=true;
+function ver(){      
+   document.getElementById("foto").innerHTML='<img src="imagen/foto1.gif"/>';
+   if(m==true){
+      document.getElementById("foto").setAttribute("class","dos");
+      m=false;
+   }else{
+      document.getElementById("foto").setAttribute("class","uno");
+      m=true;
+   }
+}
